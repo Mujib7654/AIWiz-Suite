4ef5a4379987eecf8126ebff60096f166c3c079c
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-02 23:26:53 +0530

    hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,23 @@ const userSchema = new mongoose.Schema(
     }
 );
 
+//password hashing
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+//match password
+userSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
