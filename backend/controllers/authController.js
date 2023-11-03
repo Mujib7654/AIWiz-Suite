451b7834da5cd3c1c5532ef1b338aec812178fb4
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-03 23:04:27 +0530

    first commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,40 @@
 const errorHandler = ('../middlewares/errorMiddleware');
 const userModel = require('../../models/userModel');
 const errorResponse = require('../utils/errorResponse');
-exports.registerController = async () => {};
+
+//JWT Token
+exports.sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken(res);
+    res.status(statusCode).json({
+        success : true,
+        token,
+    });
+};
+
+//REGISTER
+exports.registerController = async (req, res, next) => {
+    try {
+        const {username, email, password} = req.body;
+        //validation
+        if(!username || !email || !password){
+            return next(new errorResponse('Please fill all the fields'));
+        }
+        //existing user
+        const exisitingUser = await userModel.findOne({email});
+        if(exisitingUser){
+            return next(new errorResponse('Email is already Registered'));
+        }
+        //create new user
+        const user = await userModel.create({username, email, password});
+        this.sendToken(user, 201, res);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+//LOGIN
 exports.loginController = async () => {};
+
+//LOGOUT
 exports.logoutController = async () => {};
