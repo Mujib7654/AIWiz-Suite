fe199186b0c3fc1855fc268e2f6d1edab4f3b3a6
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-12 21:42:21 +0530

    update

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
